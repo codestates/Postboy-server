ea6cd21286397bcaf2d91e4e7583b78b875d2ba3
hsk9210 <hsk9210@gmail.com>
2020-12-22 15:31:09 +0900

    (server) 테스트용 models 개발 #12

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors"); //아직 미완
+const session = require("express-session");
+//컨트롤러는 라우터와 분리 -> 12/22 routes/users.js로 변경. app.js에서 라우터 제거 후 routes에 기능 몰빵
+//const mainController = require("../controllers");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +25,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//여기까지는 뷰를 위한 미들웨어
+
+//세션 사용.
+app.use(
+  session({
+    secret: 'SecretKey',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+//
+/*
+app.use(
+  cors({
+    //origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true
+  })
+);
+*/
+
+//주소 분기 - deprecated.
+//routes/users.js가 메인 라우터 입니다.
+//app.post("/signin", mainController.signInController);
+//app.post("/signout", mainController.signOutController);
+//
+app.use('/', indexRouter); //이건 routes/index.js 를 통해 views/index.js로 넘어갑니다.
+app.use('/controllers', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+const mainController = require("../controllers/index");
 /* GET users listing. */
+/*
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+*/
+router.post("/signin", mainController.signInController);
+router.post("/signout", mainController.signOutController);
+//추후에 필요?
+//router.get("/:id", mainController.redirect);
 
 module.exports = router;
